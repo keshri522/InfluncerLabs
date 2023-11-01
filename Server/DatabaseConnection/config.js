060adfb4bac84db92419005cd4897663c4daba2b
keshri522 <rkeshri522@gmail.com>
2023-11-01 19:19:25 +0530

    updated deps

--- Server/DatabaseConnection/config.js
+++ Server/DatabaseConnection/config.js
@@ -7,8 +7,6 @@ const DatabaseConnection = async () => {
   try {
     const DataBaseUri = `mongodb+srv://rkeshri522:${process.env.password}@cluster0.pmobmox.mongodb.net/InfluncerLab?retryWrites=true&w=majority`;
     const connect = await mongoose.connect(DataBaseUri, {
-      UseNewUrlParser: true,
-      useUnifiedTopology: true,
       // note//
       // UseNewUrlParser:ture  //When this option is enabled, it tells the MongoDB driver to use the new URL parser when connecting to the MongoDB server.
       // useUnifiedTopology: true, when we use this we  are instructing the MongoDB driver to use the unified topology engine for managing connections to the MongoDB server.
