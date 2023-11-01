c61ebbfcbbbf011ed24159d2abd05d41a5585c08
keshri522 <rkeshri522@gmail.com>
2023-11-01 20:18:17 +0530

    add

--- Server/DatabaseConnection/config.js
+++ Server/DatabaseConnection/config.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv").config();
 
 const DatabaseConnection = async () => {
   try {
-    const DataBaseUri = `mongodb+srv://rkeshri522:${process.env.password}@cluster0.pmobmox.mongodb.net/InfluncerLab?retryWrites=true&w=majority`;
+    const DataBaseUri = `mongodb+srv://rkeshri522:Rahulkeshri123@cluster0.pmobmox.mongodb.net/InfluncerLab?retryWrites=true&w=majority`;
     const connect = await mongoose.connect(DataBaseUri, {
       // note//
       // UseNewUrlParser:ture  //When this option is enabled, it tells the MongoDB driver to use the new URL parser when connecting to the MongoDB server.
